f9e1622f82ce4790f4f90e797d6a905871672e38
Łukasz Fujarski <lukasion@gmail.com>
2024-01-13 10:58:03 +0100

    Create basic form

--- src/app.js
+++ src/app.js
@@ -1,3 +1,24 @@
 export class App {
-  message = 'Hello World!';
+  constructor() {
+    this.heading = 'Fuel Calculator';
+    this.trips = [];
+    this.tripDescription = '';
+  }
+
+  addTrip() {
+    if (this.tripDescription) {
+      this.trips.push({
+        description: this.tripDescription,
+        done: false
+      });
+      this.tripDescription = '';
+    }
+  }
+
+  removeTrip(trip) {
+    const index = this.trips.indexOf(trip);
+    if (index !== -1) {
+      this.trips.splice(index, 1);
+    }
+  }
 }
